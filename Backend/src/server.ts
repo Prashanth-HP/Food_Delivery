90aa4d7e3ae4a68dd944774b585ec0864ee2ca54
sidk3 <149926270+sidk3@users.noreply.github.com>
2025-04-06 09:21:40 +0530

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -18,10 +18,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// ... after app initialization
-// app.use('/api', testRoute);
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/orders',orderRoutes);
-//in package.json
-// "start": "node --loader ts-node/esm src/server.ts",
-// "dev": "nodemon src/server.ts",
