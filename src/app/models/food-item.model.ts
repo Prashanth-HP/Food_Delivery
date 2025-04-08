bb6ad39f70d2a457f4ba6a0848f4fb5ce45c9644
Hari Prashanth R <146423955+Prashanth-HP@users.noreply.github.com>
2025-04-08 22:20:13 +0530

    Delete src directory

--- src/app/models/food-item.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FoodItem {
-  id: number;
-  name: string;
-  price: number;
-  imageUrl: string;
-  description: string;
-  category: string;   // ✅ use 'category' to describe Veg/Non-Veg
-  quantity: number;
-}
--- src/app/models/order.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Order {
-  id: number;
-  restaurantId: number;
-  items: string[];
-  totalAmount: number;
-  status: 'Pending' | 'Preparing' | 'Out for Delivery' | 'Delivered';
-}
