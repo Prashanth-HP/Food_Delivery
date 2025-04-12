64d21dcf5f0ef6402adccd68c0f97bc576414ece
Hari Prashanth R <146423955+Prashanth-HP@users.noreply.github.com>
2025-04-12 13:01:43 +0530

    Delete Capstone directory

--- Capstone/Frontend/src/app/pipes/filter-food.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterFoodPipe } from './filter-food.pipe';
-
-describe('FilterFoodPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterFoodPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
