1f0818bb96e69ef581cab001af2c5e2cf5f96725
sidk3 <149926270+sidk3@users.noreply.github.com>
2025-04-15 10:27:28 +0530

    Update dine-data.service.ts

--- Frontend/src/app/services/dine-data.service.ts
+++ Frontend/src/app/services/dine-data.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map,tap } from 'rxjs/operators'
-import { Restaurant } from 'src/app/models/restaurant.model';  // Adjust path to your model
+import { Restaurant } from 'src/app/models/restaurant.model';  
 
 @Injectable({
   providedIn: 'root',
 })
 export class DineDataService {
-  private apiUrl = 'http://localhost:3000/api/restaurants ';  // Path to the JSON file
+  private apiUrl = 'http://localhost:3000/api/restaurants ';  
 
   constructor(private http: HttpClient) {}
 
   getRestaurants(): Observable<Restaurant[]> {
       return this.http.get<Restaurant[]>(this.apiUrl).pipe(
         tap((data) => {
-          console.log('Fetched restaurants:', data);  // Add this line
+          console.log('Fetched restaurants:', data);  
         })
     );
   }
